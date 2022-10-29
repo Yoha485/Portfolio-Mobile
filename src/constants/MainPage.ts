87f660124fc7f0503b08aec261b9ff8a3de4e7bb
Anton Morozov <anton.morozov@multiparts.net>
2022-10-29 13:00:23 +0200

    code refactoring

--- src/constants/MainPage.ts
+++ src/constants/MainPage.ts
@@ -48,19 +48,5 @@ export const projects: Array<Project> = [
     description: 'Dolore duis qui aliquip consectetur ipsum ullamco ad ea veniam.',
     website: 'https://alina-barannykova.netlify.app/',
     code: 'https://github.com/Yoha485/Designer-Portfolio'
-  },
-  {
-    href: '/projects/desigher_portfolio',
-    img: '/MainPage/projects/designer_portfolio.jpg',
-    description: 'Dolore duis qui aliquip consectetur ipsum ullamco ad ea veniam.',
-    website: 'https://alina-barannykova.netlify.app/',
-    code: 'https://github.com/Yoha485/Designer-Portfolio'
-  },
-  {
-    href: '/projects/desigher_portfolio',
-    img: '/MainPage/projects/designer_portfolio.jpg',
-    description: 'Dolore duis qui aliquip consectetur ipsum ullamco ad ea veniam.',
-    website: 'https://alina-barannykova.netlify.app/',
-    code: 'https://github.com/Yoha485/Designer-Portfolio'
   }
 ];
